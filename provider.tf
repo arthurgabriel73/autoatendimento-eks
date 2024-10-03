af6e65c83536e5eadb07cae87e4821668dfe0b9f
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-03 20:59:29 -0300

    feat: migrate terraform state

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.48.0"
     }
   }
+
+  backend "s3" {
+    bucket = "autoatendimento-bucket"
+    key = "dev/terraform.tfstate"
+    region = "us-east-1"
+    
+  }
 }
 
 provider "aws" {
