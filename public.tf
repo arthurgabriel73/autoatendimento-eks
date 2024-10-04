0d09b3f098183da4525e22266d3a3fc50e4e653f
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-03 22:03:08 -0300

    feat: variables

--- public.tf
+++ public.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "eks_subnet_public_1a" {
   vpc_id            = aws_vpc.eks-vpc.id
-  cidr_block        = cidrsubnet(aws_vpc.eks-vpc.cidr_block, 8, 1)
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 1)
   availability_zone = "us-east-1a"
   tags = {
     Name = "autoatendimento-eks-subnet-public-1a"
@@ -9,7 +9,7 @@ resource "aws_subnet" "eks_subnet_public_1a" {
 
 resource "aws_subnet" "eks_subnet_public_1b" {
   vpc_id            = aws_vpc.eks-vpc.id
-  cidr_block        = cidrsubnet(aws_vpc.eks-vpc.cidr_block, 8, 2)
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 2)
   availability_zone = "us-east-1b"
   tags = {
     Name = "autoatendimento-eks-subnet-public-1b"
