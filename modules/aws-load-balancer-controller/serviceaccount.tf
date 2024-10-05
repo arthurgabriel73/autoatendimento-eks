ba53ecc3c79d38d0463d513707a807ee6ff7fdcd
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-05 03:22:51 -0300

    refactor: k8s service account

--- modules/aws-load-balancer-controller/serviceaccount.tf
+++ modules/aws-load-balancer-controller/serviceaccount.tf
@@ -3,7 +3,7 @@ resource "kubernetes_service_account" "eks_controller_sa" {
     name      = "aws-load-balancer-controller"
     namespace = "kube-system"
     annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.eks_controller_role.arn
+      "eks.amazonaws.com/role-arn" = "arn:aws:iam::${data.aws_caller_identity.current.id}:role/${aws_iam_role.eks_controller_role.name}"
     }
   }
 }
