5a0cff9b002272dbbe79f86c460ca94457172002
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-05 00:49:55 -0300

    fix: typo

--- modules/cluster/iam.tf
+++ modules/cluster/iam.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role" "eks_cluster_role" {
   )
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_role_attatchment" {
+resource "aws_iam_role_policy_attachment" "eks_cluster_role_attachment" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
