57e181483dd3a965c7ed7154943551526f99a76a
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-05 22:28:58 -0300

    feat: adds default variables

--- variables.tf
+++ variables.tf
@@ -1,19 +1,28 @@
 variable "cidr_block" {
   type        = string
   description = "Networking CIDR block to be used for the VPC"
+  default = "10.0.0.0/16"
 }
 
 variable "project_name" {
   type        = string
   description = "Project name to be used to name the resources (Name tag)"
+  default = "autoatendimento-eks"
 }
 
 variable "region" {
   type        = string
   description = "AWS region to create the resources"
+  default = "us-east-1"
 }
 
 variable "tags" {
   type        = map(any)
   description = "A map of tags to add to all AWS resources"
+  default = {
+    Department   = "SRE"
+    Organization = "Independent"
+    Project      = "AutoAtendimento-EKS"
+    Environment  = "Development"
+  }
 }
