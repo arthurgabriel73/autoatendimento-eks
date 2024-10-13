c748257cd7906ecf91f2956a9c51124e88522537
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-13 19:35:12 -0300

    feat: adds new outputs

--- output.tf
+++ output.tf
@@ -5,3 +5,15 @@ output "eks_vpc_id" {
 output "eks_sg_id" {
   value = module.eks_network.eks_sg_id
 }
+
+output "cluster_endpoint" {
+  value = module.eks-cluster.endpoint
+}
+
+output "certificate_authority" {
+  value = module.eks-cluster.certificate_authority
+}
+
+output "cluster_name" {
+  value = module.eks-cluster.cluster_name
+}
